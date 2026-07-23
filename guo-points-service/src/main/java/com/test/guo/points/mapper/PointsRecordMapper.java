f7cd2e38dfd3c180ff8ec37d38bb3af7f192b44e
Guo <guo@example.com>
2026-07-23 17:42:21 +0800

    新增积分

--- guo-points-service/src/main/java/com/test/guo/points/mapper/PointsRecordMapper.java
+++ guo-points-service/src/main/java/com/test/guo/points/mapper/PointsRecordMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Param;
 public interface PointsRecordMapper {
 
     @Insert("""
-        INSERT IGNORE INTO user_points_record(
+        INSERT IGNORE INTO user_points_record(   //IGNORE：遇到唯一键重复时，不抛出异常，忽略本次插入
                event_id,
                user_id,
                change_amount,
